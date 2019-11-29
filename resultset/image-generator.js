0eea13dbd68563450118cab4da2e2722f71b076e
si4.iot <si4.iot.ufes@gmail.com>
2019-11-29 11:58:22 -0300

    Testando passagem de urls

--- resultset/image-generator.js
+++ resultset/image-generator.js
@@ -1,55 +1,102 @@
 var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 
-// var xhr = new XMLHttpRequest();
-// var url = "http://localhost:8000/";
-// xhr.open("POST", url, true);
-// xhr.setRequestHeader("Content-Type", "application/json");
-// xhr.post("/", function (request, response) {
-//     console.log('Got request:', request);
-// });
-// xhr.send('si4-iot-tdUrlRequest');
+// Difining default used ports.
+// ATENTION: must be equally defined in both td-image-getter.js and image-generator.js files
+DEFAULT_PORT = 8000;
+// DEFAULT_URLS_SHIPPER_PORT = 7000;
 
-urls = ["coap://localhost:5683/counter", "http://localhost:8080/sensor"];
-
-urls.forEach(url => {
-	WoT.fetch(url).then(async (td) => {
-
-		let thing = WoT.consume(td);
-
-		try {
-			for (property of Object.values(thing.properties)) {
-				if (!property.writeOnly) {
-					let read = await property.read();
-					property["value"] = read;
-					// console.log(property.getName(),'value:',read);
+new Promise((resolve, reject) => {
+	var xhr = new XMLHttpRequest();
+	var url = "http://localhost:" + DEFAULT_PORT + '/url-request';
+	xhr.onreadystatechange = () => {
+		if (xhr.readyState == 4) {
+			if (xhr.status == 200) {
+				urls = JSON.parse(xhr.responseText);
+				for (u of urls) {
+					console.log(u);
 				}
+				resolve(urls);
 			}
-			image = JSON.stringify(thing);
-
-			// var mqtt = require('mqtt');
-			// var client = mqtt.connect('mqtt://test.mosquitto.org', { clientId: "si4-iot-iGen" });
-			// client.on('connect', function () {
-			// 	// client.publish('si4-iot-TD-images','TEST MESSAGE HERE!!!');
-			// 	client.publish('si4-iot-TD-images', image);
-			// 	client.end();
-			// });
-
-			// console.log('Child ended');
-
-			// var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
-
-			var xhr = new XMLHttpRequest();
-			var url = "http://localhost:8000/";
-			xhr.open("POST", url, true);
-			xhr.setRequestHeader("Content-Type", "application/json");
-			// var data = JSON.stringify({"email":"hey@mail.com", "password":"12345678"});		
-			// console.log('sending data...');
-			xhr.send(image);
-
-		} catch (err) {
-			console.error("Script error:", err);
+			else {
+				reject(xhr.statusText);
+			}
 		}
+	}
+	xhr.open("POST", url, true);
+	xhr.send('si4-iot-tdUrlRequest');
+}).then((urls) => {
+	urls.forEach(url => {
+		WoT.fetch(url).then(async (td) => {
 
-	}).catch((err) => { console.error("Fetch error:", err); });
+			let thing = WoT.consume(td);
 
-});
+			try {
+				for (property of Object.values(thing.properties)) {
+					if (!property.writeOnly) {
+						let read = await property.read();
+						property["value"] = read;
+						// console.log(property.getName(),'value:',read);
+					}
+				}
+				image = JSON.stringify(thing);
+
+				var xhr = new XMLHttpRequest();
+				var url = "http://localhost:" + DEFAULT_PORT + '/';
+				xhr.open("POST", url, true);
+				xhr.setRequestHeader("Content-Type", "application/json");
+				// console.log('sending data...');
+				xhr.send(image);
+
+			} catch (err) {
+				console.error("Script error:", err);
+			}
+
+		}).catch((err) => { console.error("Fetch error:", err); });
+	});
+}).catch((err) => { console.error(err) });
+
+// var urls = []
+
+// var xhr = new XMLHttpRequest();
+// 	var url = "http://localhost:" + DEFAULT_PORT + '/url-request';
+// 	xhr.onreadystatechange = () => {
+// 		if (xhr.readyState == 4 && xhr.status == 200) {
+// 			urls = JSON.parse(xhr.responseText);
+// 			console.log('Response:', urls);
+// 			// resolve(urls);
+// 		}
+// 	}
+// 	xhr.open("POST", url, true);
+// 	xhr.send('si4-iot-tdUrlRequest');
+
+// urls = ["coap://localhost:5683/counter", "http://localhost:8080/sensor"];
+
+// urls.forEach(url => {
+// 	WoT.fetch(url).then(async (td) => {
+
+// 		let thing = WoT.consume(td);
+
+// 		try {
+// 			for (property of Object.values(thing.properties)) {
+// 				if (!property.writeOnly) {
+// 					let read = await property.read();
+// 					property["value"] = read;
+// 					// console.log(property.getName(),'value:',read);
+// 				}
+// 			}
+// 			image = JSON.stringify(thing);
+
+// 			var xhr = new XMLHttpRequest();
+// 			var url = "http://localhost:" + DEFAULT_PORT + '/';
+// 			xhr.open("POST", url, true);
+// 			xhr.setRequestHeader("Content-Type", "application/json");
+// 			// console.log('sending data...');
+// 			xhr.send(image);
+
+// 		} catch (err) {
+// 			console.error("Script error:", err);
+// 		}
+
+// 	}).catch((err) => { console.error("Fetch error:", err); });
+
+// });
--- resultset/td-image-getter.js
+++ resultset/td-image-getter.js
@@ -7,19 +7,27 @@ CLI_PATH = '../../thingweb.node-wot/packages/cli/dist/cli.js';
 IMG_GENERATOR_PATH = 'image-generator.js';
 CLI_FLAGS = '--clientOnly';
 
-DEFAULT_IMAGE_GETTER_PORT = 8000;
-DEFAULT_URLS_SHIPPER_PORT = 7000;
+// Difining default used ports.
+// ATENTION: must be equally defined in both td-image-getter.js and image-generator.js files
+DEFAULT_PORT = 8000;
+// DEFAULT_URLS_SHIPPER_PORT = 7000;
 
-var urls = process.argv.slice(2); // getting array of urls
+// const urls = process.argv.slice(2); // getting array of urls
+const urls = ["http://localhost:8080/counter", "http://localhost:8080/sensor"];
 
 // Creating a express server to get the TDs images
 var app = express();
+app.post("/url-request", function (request, response) {
+    urls_json = JSON.stringify(urls);
+    response.json(urls_json);
+});
 app.use(myParser.json({ extended: true }));
 app.post("/", function (request, response) { // message handler
-    process.send(request.body); // sending image to parent process
+    // process.send(request.body);              // sending image to parent process
+    console.log(request.body);
 });
 console.log('Resultset awaiting TD response...');
-app.listen(DEFAULT_IMAGE_GETTER_PORT);
+app.listen(DEFAULT_PORT);
 
 // Calling thingweb.node to build the TDs images
 var child = childProcess.fork(CLI_PATH, [CLI_FLAGS, IMG_GENERATOR_PATH]);
