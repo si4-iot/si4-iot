dd506a75acee00de62c6aaf8db1961a922075a5f
si4-iot <si4.iot.ufes@gmail.com>
2019-12-03 21:44:08 -0300

    Versao 2.0.6

---
+++ build/precache-manifest.b5e9385be3686e7461406f75001a45b1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "556325d45e9c624fee1f643a68ec3db5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b1a1ec5c88efa1f037e",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "a08f86919b1427dfce85",
+    "url": "/static/css/main.ae3c9175.chunk.css"
+  },
+  {
+    "revision": "8b1a1ec5c88efa1f037e",
+    "url": "/static/js/2.de1a9915.chunk.js"
+  },
+  {
+    "revision": "a08f86919b1427dfce85",
+    "url": "/static/js/main.165f6d42.chunk.js"
+  },
+  {
+    "revision": "024acffaebdd6d77349a",
+    "url": "/static/js/runtime~main.84951066.js"
+  },
+  {
+    "revision": "9a3b0f3f209b7218c61cebe405430a33",
+    "url": "/static/media/icon.9a3b0f3f.png"
+  },
+  {
+    "revision": "c4c3467b9bc883b65f75be9ec469d6f0",
+    "url": "/static/media/logo.c4c3467b.png"
+  }
+]);
