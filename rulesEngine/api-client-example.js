b8c6b965be7fe7882190a43e07a7482d273623df
si4-iot <si4.iot.ufes@gmail.com>
2020-05-04 16:11:37 -0300

    adding api-client-example.js

---
+++ rulesEngine/api-client-example.js
@@ -0,0 +1,49 @@
+const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+const xhr = new XMLHttpRequest();
+
+// array of disired things urls
+var urls = ["http://localhost:8080/counter", "http://localhost:8080/sensor"];
+// filtering conditions (in json-rules-engines format)
+var conditions = {
+    any: [{
+        all: [{
+            fact: 'properties',
+            path: '$.temperature.value',
+            operator: 'greaterThanInclusive',
+            value: 0
+        }, {
+            fact: 'properties',
+            path: '$.temperature.value',
+            operator: 'lessThan',
+            value: 100
+        }]
+    },
+    {
+        fact: 'properties',
+        path: '$.count.value',
+        operator: 'greaterThan',
+        value: 0
+    }]
+}
+
+xhr.onreadystatechange = () => {
+    if (xhr.readyState == 4) {
+        console.log('status: ', xhr.status);
+        if (xhr.status == 200) {
+            console.log('Got response:');
+            console.log(xhr.responseText);
+        }
+        else {
+            console.log('Error: status not ok');
+            console.log('or timeout event ocured...');
+        }
+    }
+}
+
+// xhr.open("POST", "http://172.31.47.144:3000/scenes", true);
+xhr.open("POST", "http://18.216.2.4:3000/scenes", true);
+xhr.setRequestHeader('Content-Type', 'application/json');
+xhr.send(JSON.stringify({
+    urls: urls,
+    conditions: conditions
+}));
