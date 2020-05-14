1a2ae57b761b946a8bed4eda8e72eaf4a818f98e
si4-iot <si4.iot.ufes@gmail.com>
2020-05-14 14:46:25 -0300

    Updating server-updater for tests

--- rulesEngine/server-updater-example.js
+++ rulesEngine/server-updater-example.js
@@ -24,7 +24,7 @@ var conditions = {
     {
         fact: 'properties',
         path: '$.count.value',
-        operator: 'greaterThan',
+        operator: 'greaterThanInclusive',
         value: 0
     }]
 }
