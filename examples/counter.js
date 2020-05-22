54636ce9a607ff3b01f5a8432c19b424825a56c9
si4-iot <si4.iot.ufes@gmail.com>
2020-05-22 12:24:08 -0300

    Updating test examples

--- examples/counter.js
+++ examples/counter.js
@@ -1,93 +1,203 @@
 /********************************************************************************
- * Copyright (c) 2018 - 2019 Contributors to the Eclipse Foundation
- * 
+ * Copyright (c) 2018 - 2020 Contributors to the Eclipse Foundation
+ *
  * See the NOTICE file(s) distributed with this work for additional
  * information regarding copyright ownership.
- * 
+ *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0, or the W3C Software Notice and
  * Document License (2015-05-13) which is available at
  * https://www.w3.org/Consortium/Legal/2015/copyright-software-and-document.
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0 OR W3C-20150513
  ********************************************************************************/
 
-const NAME_PROPERTY_COUNT = "count";
-const NAME_PROPERTY_LAST_CHANGE = "lastChange";
-const NAME_ACTION_INCREMENT = "increment";
-const NAME_ACTION_DECREMENT = "decrement";
-const NAME_ACTION_RESET = "reset";
-const NAME_EVENT_CHANGE = "change";
-
-let thing = WoT.produce({
-		title: "counter",
-		description: "counter example Thing",
-		"@context": ["https://www.w3.org/2019/wot/td/v1", {"iot": "http://example.org/iot"}],
-	});
-
-console.log("Produced " + thing.title);
-
-thing.addProperty(
-	NAME_PROPERTY_COUNT,
-	{
-		type: "integer",
-		description: "current counter value",
-		"iot:Custom": "example annotation",
-		observable: true,
-		readOnly: true
-	},
-	0);
-thing.addProperty(
-	NAME_PROPERTY_LAST_CHANGE,
-	{
-		type: "string",
-		description: "last change of counter value",
-		observable: true,
-		readOnly: true
-	},
-	(new Date()).toISOString());
-
-thing.addAction(
-	NAME_ACTION_INCREMENT,
-	{},
-	() => {
-		console.log("Incrementing");
-		return thing.properties[NAME_PROPERTY_COUNT].read().then( (count) => {
-			let value = count + 1;
-			thing.properties[NAME_PROPERTY_COUNT].write(value);
-			thing.properties[NAME_PROPERTY_LAST_CHANGE].write((new Date()).toISOString());
-			thing.events[NAME_EVENT_CHANGE].emit();
-		});
-	});
-
-thing.addAction(
-	NAME_ACTION_DECREMENT,
-	{},
-	() => {
-		console.log("Decrementing");
-		return thing.properties[NAME_PROPERTY_COUNT].read().then( (count) => {
-			let value = count - 1;
-			thing.properties[NAME_PROPERTY_COUNT].write(value);
-			thing.properties[NAME_PROPERTY_LAST_CHANGE].write((new Date()).toISOString());
-			thing.events[NAME_EVENT_CHANGE].emit();
-		});
-	});
-
-thing.addAction(
-	NAME_ACTION_RESET,
-	{},
-	() => {
-		console.log("Resetting");
-		thing.properties[NAME_PROPERTY_COUNT].write(0);
-		thing.properties[NAME_PROPERTY_LAST_CHANGE].write((new Date()).toISOString());
-		thing.events[NAME_EVENT_CHANGE].emit();
-	});
-	
-thing.addEvent(
-	NAME_EVENT_CHANGE,
-	{});
-
-thing["support"] = "git://github.com/eclipse/thingweb.node-wot.git";
-
-thing.expose().then( () => { console.info(thing.title + " ready"); } );
+// This is an example Thing script. 
+// It has a count property that can be incremented or decremented via actions and its changes are reported via events.
+// It also has two properties that return an image. The SVG property is also influenced by the increment and decrement actions.
+// Features
+// * basic properties, actions, events
+// * uriVariables
+// * multi-language
+// * image contentTypes for properties (Note: the contentType applies to all forms of the property)
+WoT.produce({
+    title: "counter",
+    titles: {
+        "en": "counter",
+        "de": "zähler",
+        "it": "Contatore"
+    },
+    description: "counter example Thing",
+    descriptions: {
+        "en": "counter example Thing",
+        "de": "Zähler Beispiel Ding",
+        "it": "Contatore Esempio"
+    },
+    support: "git://github.com/eclipse/thingweb.node-wot.git",
+    "@context": ["https://www.w3.org/2019/wot/td/v1", { "iot": "http://example.org/iot" }],
+    properties: {
+        count: {
+            type: "integer",
+            description: "current counter value",
+            descriptions: {
+                "en": "current counter value",
+                "de": "Derzeitiger Zähler Stand",
+                "it": "valore attuale del contatore"
+            },
+            "iot:Custom": "example annotation",
+            observable: true,
+            readOnly: true
+        },
+        countAsImage: {
+            description: "current counter value as SVG image",
+            forms: [{
+                    contentType: "image/svg+xml"
+                }],
+            observable: false,
+            readOnly: true,
+            uriVariables: {
+                fill: { "type": "string" }
+            }
+        },
+        redDotImage: {
+            description: "Red dot image as PNG",
+            forms: [{
+                    contentType: "image/png"
+                }],
+            observable: false,
+            readOnly: true
+        },
+        lastChange: {
+            type: "string",
+            description: "last change of counter value",
+            descriptions: {
+                "en": "last change of counter value",
+                "de": "Letzte Änderung",
+                "it": "ultima modifica del valore"
+            },
+            observable: true,
+            readOnly: true
+        }
+    },
+    actions: {
+        increment: {
+            description: "Incrementing counter value (with optional step parameter as uriVariable)",
+            descriptions: {
+                "en": "increment value",
+                "de": "Zähler erhöhen",
+                "it": "incrementare valore"
+            },
+            uriVariables: {
+                step: { "type": "integer", "minimum": 1, "maximum": 250 }
+            }
+        },
+        decrement: {
+            description: "Decrementing counter value (with optional step parameter as uriVariable)",
+            descriptions: {
+                "en": "decrement value",
+                "de": "Zähler verringern",
+                "it": "decrementare valore"
+            },
+            uriVariables: {
+                step: { "type": "integer", "minimum": 1, "maximum": 250 }
+            }
+        },
+        reset: {
+            description: "Resetting counter value",
+            descriptions: {
+                "en": "Resetting counter value",
+                "de": "Zähler resettieren",
+                "it": "resettare valore"
+            }
+        }
+    },
+    events: {
+        change: {
+            description: "change event",
+            descriptions: {
+                "en": "change event",
+                "de": "Änderungsnachricht",
+                "it": "resettare valore"
+            }
+        }
+    }
+})
+    .then((thing) => {
+    console.log("Produced " + thing.getThingDescription().title);
+    // init property values
+    thing.writeProperty("count", 0);
+    thing.writeProperty("lastChange", (new Date()).toISOString());
+    thing.setPropertyReadHandler("countAsImage", (options) => {
+        return thing.readProperty("count").then((count) => {
+            return new Promise((resolve, reject) => {
+                let fill = "black";
+                if (options && typeof options === 'object' && 'uriVariables' in options) {
+                    console.log("options = " + JSON.stringify(options));
+                    if ('fill' in options['uriVariables']) {
+                        let uriVariables = options['uriVariables'];
+                        fill = uriVariables['fill'];
+                    }
+                }
+                resolve("<svg xmlns='http://www.w3.org/2000/svg' height='30' width='200'>" +
+                    "<text x='0' y='15' fill='" + fill + "'>" + count + "</text>" +
+                    "</svg>");
+            });
+        });
+    });
+    thing.setPropertyReadHandler("redDotImage", () => {
+        return new Promise((resolve, reject) => {
+            // data:image/png;base64,
+            resolve("iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==");
+        });
+    });
+    // set action handlers
+    thing.setActionHandler("increment", (params, options) => {
+        return thing.readProperty("count").then((count) => {
+            let step = 1;
+            if (options && typeof options === 'object' && 'uriVariables' in options) {
+                console.log("options = " + JSON.stringify(options));
+                if ('step' in options['uriVariables']) {
+                    let uriVariables = options['uriVariables'];
+                    step = uriVariables['step'];
+                }
+            }
+            let value = count + step;
+            console.log("Incrementing count from " + count + " to " + value + " (with step " + step + ")");
+            thing.writeProperty("count", value);
+            thing.writeProperty("lastChange", (new Date()).toISOString());
+            thing.emitEvent("change", value);
+        });
+    });
+    thing.setActionHandler("decrement", (params, options) => {
+        return thing.readProperty("count").then((count) => {
+            let step = 1;
+            if (options && typeof options === 'object' && 'uriVariables' in options) {
+                console.log("options = " + JSON.stringify(options));
+                if ('step' in options['uriVariables']) {
+                    let uriVariables = options['uriVariables'];
+                    step = uriVariables['step'];
+                }
+            }
+            let value = count - step;
+            console.log("Decrementing count from " + count + " to " + value + " (with step " + step + ")");
+            thing.writeProperty("count", value);
+            thing.writeProperty("lastChange", (new Date()).toISOString());
+            thing.emitEvent("change", value);
+        });
+    });
+    thing.setActionHandler("reset", () => {
+        return new Promise((resolve, reject) => {
+            console.log("Resetting count");
+            thing.writeProperty("count", 0);
+            thing.writeProperty("lastChange", (new Date()).toISOString());
+            thing.emitEvent("change", 0);
+            resolve();
+        });
+    });
+    // expose the thing
+    thing.expose().then(() => { console.info(thing.getThingDescription().title + " ready"); });
+})
+    .catch((e) => {
+    console.log(e);
+});
--- examples/sensor.js
+++ examples/sensor.js
@@ -1,68 +1,66 @@
-let sensor = WoT.produce({
+WoT.produce({
     title: "sensor",
-    description: "A multipurpose sensor. Can measure temperature, luminosity and humidity",
+    description: "A very simple test case of a multipurpose sensor. Can 'measure' temperature, luminosity and humidity",
+    properties: {
+        temperature: {
+            type: "float",
+            description: "temperature measure",
+            "unit of measurement": "degrees Celsius",
+            observable: true,
+            readOnly: true,
+        },
+        luminosity: {
+            type: "float",
+            description: "luminosity measure",
+            "unit of measurement": "lux",
+            observable: true,
+            readOnly: true
+        },
+        humidity: {
+            type: "float",
+            description: "humidity measure",
+            observable: true,
+            readOnly: true
+        }
+    },
+    actions: {
+        change_temperature: {
+            description: "ramdomly changes temperature value",
+            uriVariables: {
+                step: { "type": "float", "minimum": 1, "maximum": 100 }
+            }
+        },
+        change_luminosity: {
+            description: "ramdomly changes luminosity value",
+            uriVariables: {
+                step: { "type": "float", "minimum": 1, "maximum": 800 }
+            }
+        },
+        change_humidity: {
+            description: "ramdomly changes humidity value",
+            uriVariables: {
+                step: { "type": "float", "minimum": 1, "maximum": 200 }
+            }
+        }
+    }
+}).then((thing) => {
+    console.log("Produced " + thing.getThingDescription().title);
+    // init property values
+    thing.writeProperty("temperature", 0);
+    thing.writeProperty("luminosity", 0);
+    thing.writeProperty("humidity", 0);
+    // set action handlers
+    thing.setActionHandler("change_temperature", (params, options) => {
+        return thing.writeProperty("temperature", Math.random() * 100);
+    });
+    thing.setActionHandler("change_luminosity", (params, options) => {
+        return thing.writeProperty("luminosity", Math.random() * 800);
+    });
+    thing.setActionHandler("change_humidity", (params, options) => {
+        return thing.writeProperty("humidity", Math.random() * 200);
+    });
+    // expose the thing
+    thing.expose().then(() => { console.info(thing.getThingDescription().title + " ready"); });
+}).catch((e) => {
+    console.log(e);
 });
-
-sensor.addProperty(
-    "temperature",
-    {
-        type: "float",
-        description: "temperature measure",
-        "unit of measurement": "degrees Celsius",
-        observable: true,
-        readOnly: true,
-    },
-    0
-);
-
-sensor.addProperty(
-    "luminosity",
-    {
-        type: "float",
-        description: "luminosity measure",
-        "unit of measurement": "lux",
-        observable: true,
-        readOnly: true
-    },
-    0
-);
-
-sensor.addProperty(
-    "humidity",
-    {
-        type: "float",
-        description: "humidity measure",
-        observable: true,
-        readOnly: true
-    },
-    0
-);
-
-sensor.addAction(
-    "change_temperature",
-    {},
-    () => {
-        console.log("Changing temperature");
-        return sensor.properties["temperature"].write(Math.random()*100);
-    }
-);
-
-sensor.addAction(
-    "change_luminosity",
-    {},
-    () => {
-        console.log("Changing luminosity");
-        return sensor.properties["luminosity"].write(Math.random()*800);
-    }
-);
-
-sensor.addAction(
-    "change_humidity",
-    {},
-    () => {
-        console.log("Changing humidity");
-        return sensor.properties["humidity"].write(Math.random()*200);
-    }
-);
-
-sensor.expose()
